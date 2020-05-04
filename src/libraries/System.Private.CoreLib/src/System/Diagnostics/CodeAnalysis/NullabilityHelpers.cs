6966e55f52a7aae0617a5d3de92a368e9c23610c
Levi Broderick <levib@microsoft.com>
2020-05-03 18:24:24 -0700

    First run

---
+++ src/libraries/System.Private.CoreLib/src/System/Diagnostics/CodeAnalysis/NullabilityHelpers.cs
@@ -0,0 +1,22 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Internal.Runtime.CompilerServices;
+
+namespace System.Diagnostics.CodeAnalysis
+{
+    internal static class NullabilityHelpers
+    {
+        /// <summary>
+        /// Suppresses CS8777: "Parameter 'name' must have a non-null value when exiting."
+        /// Used when a method accepts a 'ref' parameter that should be non-null on
+        /// method exit.
+        /// </summary>
+        [Conditional("DEBUG")]
+        public static void SuppressNonNullAssignmentWarning<T>([NotNull] ref T value)
+        {
+            Unsafe.SkipInit(out value!);
+        }
+    }
+}
